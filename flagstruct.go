36b35140b445543862ef934edd7365233c53a739
Toan Nguyen <ngdinhtoan@gmail.com>
2015-10-06 21:24:41 +0700

    Ignore anonymous field.

--- flagstruct.go
+++ flagstruct.go
@@ -5,8 +5,6 @@ import (
 	"flag"
 	"os"
 	"reflect"
-	"strconv"
-	"strings"
 	"unsafe"
 )
 
@@ -18,71 +16,6 @@ type fieldData struct {
 	field reflect.StructField
 	value reflect.Value
 	tag   tagData
-}
-
-// tagData contains information about flag name, default value, usage string
-type tagData struct {
-	name     string // name of flag option
-	defValue string // default value of flag option
-	usage    string // usage of flag option
-}
-
-func (td tagData) intValue() int {
-	if td.defValue == "" {
-		return 0
-	}
-
-	i64, _ := strconv.ParseInt(td.defValue, 0, 32)
-	return int(i64)
-}
-
-func (td tagData) int64Value() int64 {
-	if td.defValue == "" {
-		return 0
-	}
-
-	i64, _ := strconv.ParseInt(td.defValue, 0, 64)
-	return i64
-}
-
-func (td tagData) uintValue() uint {
-	if td.defValue == "" {
-		return 0
-	}
-
-	ui64, _ := strconv.ParseUint(td.defValue, 0, 32)
-	return uint(ui64)
-}
-
-func (td tagData) uint64Value() uint64 {
-	if td.defValue == "" {
-		return 0
-	}
-
-	ui64, _ := strconv.ParseUint(td.defValue, 0, 64)
-	return ui64
-}
-
-func (td tagData) stringValue() string {
-	return td.defValue
-}
-
-func (td tagData) float64Value() float64 {
-	if td.defValue == "" {
-		return 0.0
-	}
-
-	f64, _ := strconv.ParseFloat(td.defValue, 64)
-	return f64
-}
-
-func (td tagData) boolValue() bool {
-	if td.defValue == "" {
-		return false
-	}
-
-	b, _ := strconv.ParseBool(td.defValue)
-	return b
 }
 
 // Parse properties of struct to flag,
@@ -131,25 +64,6 @@ func registerFlagStruct(i interface{}, fs *flag.FlagSet) {
 	}
 }
 
-func parseTag(tag string) tagData {
-	td := tagData{}
-	if tag == "" || tag == "-" {
-		return td
-	}
-
-	tagOpt := strings.Split(tag, ",")
-	td.name = tagOpt[0]
-
-	if len(tagOpt) > 2 {
-		td.usage = tagOpt[2]
-		td.defValue = tagOpt[1]
-	} else if len(tagOpt) > 1 {
-		td.defValue = tagOpt[1]
-	}
-
-	return td
-}
-
 func strctVal(i interface{}) reflect.Value {
 	v := reflect.ValueOf(i)
 
@@ -173,10 +87,11 @@ func structFields(i interface{}) []fieldData {
 	for i := 0; i < t.NumField(); i = i + 1 {
 		field := t.Field(i)
 
-		// we can't access the value of unexported fields
-		if field.PkgPath != "" {
+		// we can't access the value of unexported or anonymous fields
+		if field.PkgPath != "" || field.Anonymous {
 			continue
 		}
+
 		// don't check if it's omitted
 		var tag string
 		if tag = field.Tag.Get(tagName); tag == "-" || tag == "" {
---
+++ tag.go
@@ -0,0 +1,90 @@
+package flagstruct
+
+import (
+	"strconv"
+	"strings"
+)
+
+// tagData contains information about flag name, default value, usage string
+type tagData struct {
+	name     string // name of flag option
+	defValue string // default value of flag option
+	usage    string // usage of flag option
+}
+
+func (td tagData) intValue() int {
+	if td.defValue == "" {
+		return 0
+	}
+
+	i64, _ := strconv.ParseInt(td.defValue, 0, 32)
+	return int(i64)
+}
+
+func (td tagData) int64Value() int64 {
+	if td.defValue == "" {
+		return 0
+	}
+
+	i64, _ := strconv.ParseInt(td.defValue, 0, 64)
+	return i64
+}
+
+func (td tagData) uintValue() uint {
+	if td.defValue == "" {
+		return 0
+	}
+
+	ui64, _ := strconv.ParseUint(td.defValue, 0, 32)
+	return uint(ui64)
+}
+
+func (td tagData) uint64Value() uint64 {
+	if td.defValue == "" {
+		return 0
+	}
+
+	ui64, _ := strconv.ParseUint(td.defValue, 0, 64)
+	return ui64
+}
+
+func (td tagData) stringValue() string {
+	return td.defValue
+}
+
+func (td tagData) float64Value() float64 {
+	if td.defValue == "" {
+		return 0.0
+	}
+
+	f64, _ := strconv.ParseFloat(td.defValue, 64)
+	return f64
+}
+
+func (td tagData) boolValue() bool {
+	if td.defValue == "" {
+		return false
+	}
+
+	b, _ := strconv.ParseBool(td.defValue)
+	return b
+}
+
+func parseTag(tag string) tagData {
+	td := tagData{}
+	if tag == "" || tag == "-" {
+		return td
+	}
+
+	tagOpt := strings.Split(tag, ",")
+	td.name = tagOpt[0]
+
+	if len(tagOpt) > 2 {
+		td.usage = tagOpt[2]
+		td.defValue = tagOpt[1]
+	} else if len(tagOpt) > 1 {
+		td.defValue = tagOpt[1]
+	}
+
+	return td
+}
